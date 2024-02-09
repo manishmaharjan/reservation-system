fc5c75358440e8038925b6cff4096aa01474bae8
manexsora <manexsora@gmail.com>
2024-02-09 17:01:30 +0200

    App fixes

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from models import User, Room, Reservation, Facility
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///reservation_system.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
-
 
 
 
