198d378f17780da6ec45c27f89542a58b66e55c4
manexsora <manexsora@gmail.com>
2024-02-09 12:46:40 +0200

    "Add models"

---
+++ models.py
@@ -0,0 +1,41 @@
+from app import db
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    reservations = db.relationship('Reservation', back_populates='user')
+
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    room_name = db.Column(db.String(100), unique=True, nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+
+    reservations = db.relationship('Reservation', back_populates ='room')
+class Reservation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    start_time = db.Column(db.Time, nullable=False)
+    end_time = db.Column(db.Time, nullable=False)
+
+    __table_args__ = (
+        db.UniqueConstraint('room_id', 'date', 'start_time', 'end_time'),
+    )
+
+    room = db.relationship('Room', back_populates = 'reservations')
+    user = db.relationship('User', back_populates = 'reservations')
+
+
+
