38bb7402b0bfc812e84f0eb84b484c01ca6820d5
manexsora <manexsora@gmail.com>
2024-02-20 12:42:00 +0200

    Password deleted

--- src/models.py
+++ src/models.py
@@ -12,7 +12,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
 
     reservations = db.relationship('Reservation', back_populates='user', cascade='all, delete-orphan')
 
