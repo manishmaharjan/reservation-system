fc49e848cc85c0af7037fdb9383ee7718694b470
manexsora <manexsora@gmail.com>
2024-02-21 19:46:43 +0200

    Decorators.py file created

--- src/api.py
+++ src/api.py
@@ -1,37 +1,11 @@
-from http.client import FORBIDDEN
 from flask import Flask, request, Response
 from flask_restful import Api, Resource
 from src import create_app, db
-from src.models import *
-from functools import wraps
-
+from src.models import ApiKey,Room,Reservation,User
+from src.decorators import require_user, require_admin
 app = create_app()
 
 api = Api(app)
-
-# Function to verify if the request comes from an admin righted user
-def require_admin(func):
-    def wrapper(*args, **kwargs):
-        # The token will go in a special header named: "Api-key"
-        key_hash = ApiKey.key_hash(request.headers.get("Api-key").strip())
-        db_key = ApiKey.query.filter_by(key=key_hash).first()
-        if db_key and db_key.admin:
-            return func(db.key.user, *args, **kwargs)
-        raise FORBIDDEN
-    return wrapper
-
-# Function to verify if the request comes from an actual user
-def require_user(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        # The token will go in a special header named: "Api-key"
-        key_hash = ApiKey.key_hash(request.headers.get("Api-key").strip())
-        db_key = ApiKey.query.filter_by(key= key_hash).first()
-        if db_key:
-            kwargs['user'] = db_key.user
-            return func(*args, **kwargs)
-        raise FORBIDDEN
-    return wrapper
 
 class GetReservations(Resource):
     @require_user
---
+++ src/decorators.py
@@ -0,0 +1,28 @@
+from functools import wraps
+from http.client import FORBIDDEN
+from flask import request
+from src.models import *
+
+# Function to verify if the request comes from an admin righted user
+def require_admin(func):
+    def wrapper(*args, **kwargs):
+        # The token will go in a special header named: "Api-key"
+        key_hash = ApiKey.key_hash(request.headers.get("Api-key").strip())
+        db_key = ApiKey.query.filter_by(key=key_hash).first()
+        if db_key and db_key.admin:
+            return func(db.key.user, *args, **kwargs)
+        raise FORBIDDEN
+    return wrapper
+
+# Function to verify if the request comes from an actual user
+def require_user(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # The token will go in a special header named: "Api-key"
+        key_hash = ApiKey.key_hash(request.headers.get("Api-key").strip())
+        db_key = ApiKey.query.filter_by(key= key_hash).first()
+        if db_key:
+            kwargs['user'] = db_key.user
+            return func(*args, **kwargs)
+        raise FORBIDDEN
+    return wrapper
