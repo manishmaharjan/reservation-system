e09b84e5c3b0e168e136744765596d7b5f0e0297
manexsora <manexsora@gmail.com>
2024-02-27 18:39:53 +0200

    RoomConverter added

--- src/api.py
+++ src/api.py
@@ -1,11 +1,13 @@
-from flask import Flask, request, Response
-from flask_restful import Api, Resource
+from flask_restful import Api
 from src import create_app, db
 from .resources import reservation, user
-
+from converters import RoomConverter
 app = create_app()
 
 api = Api(app)
 
+app.url_map.converters["room"] = RoomConverter
+
 api.add_resource(user.RegisterUser, "/api/user/register/")
 api.add_resource(reservation.GetReservations, "/api/reservations/")
+api.add_resource(reservation.CreateReservation, "/api/reservations/<room:room>/")
---
+++ src/converters.py
@@ -0,0 +1,13 @@
+from werkzeug.routing import BaseConverter
+from models import Room
+from werkzeug.exceptions import NotFound
+
+class RoomConverter(BaseConverter):
+    def to_python(self, room_name):
+        db_room = Room.query.filter_by(room_name=room_name).first()
+        if db_room is None:
+            raise NotFound
+        return db_room
+        
+    def to_url(self, db_room):
+        return db_room.room_name
