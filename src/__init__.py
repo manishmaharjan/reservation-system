59d3834c35304b59869b1548efc93b4eaad3eec6
Manish Maharjan <developermanishid@gmail.com>
2024-03-01 15:58:12 +0200

    updated pylint and docs

--- src/__init__.py
+++ src/__init__.py
@@ -1,28 +1,42 @@
+"""
+import modules
+"""
 import os
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
 db = SQLAlchemy()
 
 # Based on http://flask.pocoo.org/docs/1.0/tutorial/factory/#the-application-factory
 # Modified to use Flask SQLAlchemy
 def create_app(test_config=None):
+    """
+    Create and configure the Flask application.
+
+    Args:
+        test_config (dict, optional):
+        Configuration dictionary for testing purposes. Defaults to None.
+
+    Returns:
+        Flask: The configured Flask application.
+    """
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
-        SQLALCHEMY_DATABASE_URI="sqlite:///" + os.path.join(app.instance_path, "reservation_system.db"),
+        SQLALCHEMY_DATABASE_URI="sqlite:///" + os.path.join(app.instance_path,
+                                                            "reservation_system.db"),
         SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
-    
+
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
     else:
         app.config.from_mapping(test_config)
-        
+
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    
+
     db.init_app(app)
     return app
