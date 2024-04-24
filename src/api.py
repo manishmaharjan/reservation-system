76d33617e0e32965fc70b26171a934cb60cf2bcd
amaelkesteman <147324865+amaelkesteman@users.noreply.github.com>
2024-04-24 17:53:38 +0300

    Update api.py
    Update for the linting tool

--- src/api.py
+++ src/api.py
@@ -4,26 +4,20 @@ This module provides the API endpoints for the reservation system.
 
 from flasgger import Swagger
 from flask_restful import Api
-
-from src import create_app
-
+from flask import Blueprint
+from .resources import reservation,user
 from .converters import RoomConverter
-from .resources import reservation, user
 
-app = create_app()
+def setup_routes(app):
+    " Initialize API with Swagger documentation"
+    swagger = Swagger(app)
+    api = Api(app)
 
-swagger = Swagger(app)
+    # Adding custom URL converters
+    app.url_map.converters["room"] = RoomConverter
 
-api = Api(app)
-
-app.url_map.converters["room"] = RoomConverter
-
-api.add_resource(user.RegisterUser, "/api/user/register/")
-api.add_resource(reservation.GetReservations, "/api/reservations/")
-api.add_resource(reservation.CreateReservation, "/api/reservations/<room:room>/")
-api.add_resource(
-    reservation.DeleteReservation, "/api/reservations/<room:room>/<int:reservation_id>/"
-)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    # Registering API resources
+    api.add_resource(user.RegisterUser, "/api/user/register/")
+    api.add_resource(reservation.GetReservations, "/api/reservations/")
+    api.add_resource(reservation.CreateReservation, "/api/reservations/<room:room>/")
+    api.add_resource(reservation.DeleteReservation, "/api/reservations/<room:room>/<int:reservation_id>/")
