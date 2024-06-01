9a743a9c00c6be0164d634aa16d0c8356ef658f4
Manexsora <manexsora@gmail.com>
2024-06-01 19:03:28 +0200

    Create ReservationCollection class

--- src/api.py
+++ src/api.py
@@ -8,7 +8,7 @@ from flask_restful import Api
 from src import create_app
 
 from .converters import RoomConverter
-from .resources import reservation, userCollection, user
+from .resources import reservation, reservationCollection,  userCollection, user
 
 app = create_app()
 
@@ -20,11 +20,10 @@ app.url_map.converters["room"] = RoomConverter
 
 api.add_resource(userCollection.UserCollection, "/api/user/")
 api.add_resource(user.UserId, "/api/user/<userId>/")
-api.add_resource(reservation.GetReservations, "/api/reservations/")
+
+api.add_resource(reservationCollection.ReservationCollection, "/api/user/<userId>/reservation/")
 api.add_resource(reservation.CreateReservation, "/api/reservations/<room:room>/")
-api.add_resource(
-    reservation.DeleteReservation, "/api/reservations/<room:room>/<int:reservation_id>/"
-)
+api.add_resource(reservation.DeleteReservation, "/api/reservations/<room:room>/<int:reservation_id>/")
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ src/resources/reservationCollection.py
@@ -0,0 +1,6 @@
+from flask import Response, request
+from flask_restful import Resource
+
+
+class ReservationCollection(Resource):
+    
